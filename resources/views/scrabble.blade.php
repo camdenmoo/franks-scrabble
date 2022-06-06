e0937665be912d951196f36f13f313cf4911e721
frank jones <frankjones.web@gmail.com>
2022-06-06 19:37:55 +0100

    Bootstrap 5 added. Model & Controller created. Route updated.

---
+++ resources/views/scrabble.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>Frank's Scrabble Game</title>
+
+</head>
+<body>
+
+    <header>
+        <div class="container">
+            <h1>Frank's Scrabble Game</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <p>Let's play the game here.</p>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">
+            Frank's Scrabble Game &copy; 2022
+        </div>
+    </footer>
+
+    
+</body>
+</html>
